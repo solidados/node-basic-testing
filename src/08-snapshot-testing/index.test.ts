ee41026f137239ea41e52d0645b9429021f6982e
solidados <geekbrains.pk@internet.ru>
2024-10-17 01:53:39 +0400

    feat: 08-snapshot-testing implemented

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,55 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
-describe('generateLinkedList', () => {
+describe('generateLinkedList', (): void => {
   // Check match by expect(...).toStrictEqual(...)
-  test('should generate linked list from values 1', () => {
-    // Write your test here
+  interface Actor {
+    id: number;
+    name: string;
+    role: string;
+  }
+
+  const FifthElementLeadingActors: Actor[] = [
+    { id: 1, name: 'Milla Jovovich', role: 'Leeloo' },
+    { id: 2, name: 'Bruce Willis', role: 'Korben Dallas' },
+    { id: 3, name: 'Gary Oldman', role: 'Jean Baptiste-Emanuelle Zorg' },
+    { id: 4, name: 'Chris Tucker', role: 'Ruby Rhod' },
+  ];
+  const FifthElementSupportingActors: Actor[] = [
+    { id: 5, name: 'Maiwenn', role: 'Diva Pavalaguna' },
+    { id: 6, name: 'Ian Holm', role: 'Vito Cornelius' },
+    { id: 7, name: 'Tommy Lister Jr', role: 'President Lindberg' },
+    { id: 8, name: 'Brion James', role: 'General Munro' },
+  ];
+
+  const expectedLinkedList = {
+    value: { id: 1, name: 'Milla Jovovich', role: 'Leeloo' },
+    next: {
+      value: { id: 2, name: 'Bruce Willis', role: 'Korben Dallas' },
+      next: {
+        value: {
+          id: 3,
+          name: 'Gary Oldman',
+          role: 'Jean Baptiste-Emanuelle Zorg',
+        },
+        next: {
+          value: { id: 4, name: 'Chris Tucker', role: 'Ruby Rhod' },
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    },
+  };
+
+  test('should generate linked list from values 1', (): void => {
+    expect(generateLinkedList(FifthElementLeadingActors)).toStrictEqual(
+      expectedLinkedList,
+    );
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(FifthElementSupportingActors)).toMatchSnapshot();
   });
 });
